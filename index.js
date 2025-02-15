784fcb88cabde6acc0fa83c56affd12516da0ee7
syncshubham <syncshubham@gmail.com>
2025-02-15 12:23:18 +0530

    players structured setup for stopping at ground, with gravity implementation for perfect landing

--- index.js
+++ index.js
@@ -6,28 +6,38 @@ canvas.height = 576;
 
 c.fillRect(0, 0, canvas.width, canvas.height);
 
+const gravity = 1.2;
+
 class Sprite
 {
     constructor({position, velocity})
     {
         this.position = position;
         this.velocity = velocity;
+        this.height = 150;
     }   
 
     draw(){
         c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 50, 150)
+        c.fillRect(this.position.x, this.position.y, 50, this.height)
     }
 
     update(){
         this.draw()
-        this.position.y += 10;
-    }
+        this.position.y += this.velocity.y;
+        if(this.position.y + this.height + this.velocity.y >= canvas.height )
+        {
+            this.velocity.y  =0;
+        }else{
+            this.velocity.y += gravity;
+        }
 
-    movesprite(){
+        console.log("vel", this.velocity.y);
+        console.log("pos", this.position.y);
     }
-
 }
+
+const villian = new Sprite({})
 
 const player = new Sprite({
     position: {
