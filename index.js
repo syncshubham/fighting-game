e64942a3f24f831bff85e0da69e8bf8cdb59e3c2
syncshubham <syncshubham@gmail.com>
2025-02-28 23:59:12 +0530

    player health bar positioning and set 100 percent health and decrementing on every attack

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ class Sprite
         };
         this.color = color;
         this.isAttacking;
+        this.health = 100
     }
 
     draw(){
@@ -157,7 +158,8 @@ function animate()
     if(rectangularCollison({rectangle1: player, rectangle2: enemy}) && player.isAttacking)
     {
         player.isAttacking = false;
-        console.log("player attack area")
+        enemy.health -= 20;
+        document.querySelector('#enemyHealth').style.width = enemy.health + "%";
     }
 
     if(rectangularCollison({rectangle1: enemy, rectangle2: player}) && enemy.isAttacking)
